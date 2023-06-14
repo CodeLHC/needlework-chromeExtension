22a46f6d2da9fdae9839e7ccb147a709d2f0c682
CodeLHC <lhcrawford97@gmail.com>
2023-06-14 09:49:40 +0100

    added event listener skeleton

---
+++ index.js
@@ -0,0 +1,12 @@
+let myPatterns = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+myPatterns.push(inputEl.value)
+})
+
+
+for (let i=0; i < myPatterns.length; i++) {
+    
+}
