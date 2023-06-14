8be369c92d5b3ce50993e810b0a9ab59dc357db3
CodeLHC <lhcrawford97@gmail.com>
2023-06-14 11:08:11 +0100

    function to render text from input field

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 let myPatterns = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
 myPatterns.push(inputEl.value)
+renderPatternLinks()
 })
 
-
-for (let i=0; i < myPatterns.length; i++) {
-    
+function renderPatternLinks() {
+    let listPatterns = ""
+    for (let i=0; i < myPatterns.length; i++) {
+        listPatterns += "<li>" + myPatterns[i] + "</li>"
+    }
+    ulEl.innerHTML = listPatterns
 }
+
+
+
